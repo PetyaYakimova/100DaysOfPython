b52a6c5c140cc1fc1aaf2adb8cf24dc0bd815d8c
Petya Yakimova <petya_yakimova@abv.bg>
2025-04-14 12:15:48 +0300

    Day 2 tasks added

---
+++ day002/task1.py
@@ -0,0 +1,16 @@
+print("Hello"[0])
+print("Hello"[-1])
+
+# Strings
+print("12" + "34")
+
+# Integers
+print(12 + 34)
+print(123_456_789)
+
+# Float
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
