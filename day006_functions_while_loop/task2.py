d0e5f3056eb54797f9eb9dd215688d0472929359
Petya Yakimova <petya_yakimova@abv.bg>
2025-04-21 10:51:00 +0300

    Day 6 tasks added

---
+++ day006_functions_while_loop/task2.py
@@ -0,0 +1,6 @@
+count_repeated = int(input("How many times do you want to be saluted? "))
+
+current_salute_count = 0
+while current_salute_count < count_repeated:
+    print("Hello!")
+    current_salute_count += 1
